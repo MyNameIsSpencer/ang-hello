c768d4b8750bb603ecf87aaeea5985b8c61579dd
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-01-17 14:46:03 -0500

    add url and fix selector bug

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,20 +5,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'courses', // courses
   template: `
-    <h2>{{ getTitle() }}</h2>
+    <h2>{{ title }}</h2>
     <ul>
       <li *ngFor="let course of courses">
         {{ course }}
       </li>
     </ul>
+
+    <img src="{{ imageUrl }}" />
+    <img [src]="imageUrl" />
     `
 })
 
 export class CoursesComponent {
   title = "List of Courses";
-  courses;
+  // courses;
+  imageUrl = "http://lorempixel.com/400/200";
 
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
   }
 }
+
+
+
+
+// <h2>{{ getTitle() }}</h2>
