a8c990f6401b24f1d4fdbe75832fbf5cfb85fcc7
spencer kyonka <spencer.kyonka@hotmail.ca>
2019-01-16 23:54:04 -0500

    correct gibberish

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'courses', // courses
-  template: '<h2>{{ getTitle()asdfasfafsdasfd }}</h2>'
+  template: '<h2>{{ getTitle() }}</h2>'
 })
 export class CoursesComponent {
   title = "List of Courses";
